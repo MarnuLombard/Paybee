e8283611722ce527e02ed4f14687dd7fcfb53568
Marnu Lombard <marnulombard@gmail.com>
2019-05-28 13:01:47 +0200

    [create] Add test to check that our telegram endpoint is alive and well.

---
+++ tests/Feature/BotEndPointTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BotEndPointTest extends TestCase
+{
+    /**
+     * A *very* simple test to check that our endpoint registered with telegram is alive and well
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->post('/bot', []);
+
+        $response->assertStatus(200);
+    }
+}
