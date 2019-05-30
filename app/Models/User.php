0022a454049c153cddf41991ab6b6fbb27392525
Marnu Lombard <marnulombard@gmail.com>
2019-05-30 15:30:46 +0200

    [change] Fill out all the class members for `User` and add relationship to `Message`[]

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,21 +28,16 @@ class User extends Authenticatable
     use Notifiable;
     use SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
+    protected $guarded = ['id'];
+    protected $hidden = ['password', 'remember_token'];
+    protected $casts = [
+        'id' => 'int',
+        'email_verified_at' => 'datetime',
+        'sender_id' => 'int',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
 }
