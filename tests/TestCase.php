656c524eb3c9394fb04c8d894973493dd024306c
Marnu Lombard <marnulombard@gmail.com>
2019-05-31 11:24:24 +0200

    [change] Rather use physical sqlite file - When we do an http request to the app, we don't have all the `testing` env values - The database connection will be the default defined (mysql) - If we use `:memory:` as the `DB_DATABASE` it will query the mysql connection using that as the database when calling `assertDatabaseHas()`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        // Can't use `config()` to get path as Laravel is not yet booted
+        $filePath = __DIR__.'/../database/database.sqlite';
+        if (!file_exists($filePath)) {
+            touch($filePath);
+        }
+        parent::setUp();
+    }
+
 }
