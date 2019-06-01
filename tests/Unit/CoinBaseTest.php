61575a236be0fde6c17818051b7316e8247b8578
Marnu Lombard <marnulombard@gmail.com>
2019-06-01 06:53:29 +0200

    [change] Add test to check tyat we get results when trying to convert a currency

--- tests/Unit/CoinBaseTest.php
+++ tests/Unit/CoinBaseTest.php
@@ -11,4 +11,15 @@ class CoinBaseTest extends TestCase
     {
         $this->assertInstanceOf(Converter::class, app('\Converter'));
     }
+
+    public function testConverterGetsData()
+    {
+        /** @var Converter $converter */
+        $converter = app('\Converter');
+        $btc = $converter->toBtc(50, 'ZAR');
+        $currency = $converter->toCurrency('USD', 50);
+
+        $this->assertIsFloat($btc);
+        $this->assertIsFloat($currency);
+    }
 }
