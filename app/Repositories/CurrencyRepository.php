a735b71c6506299deaeab50d2ae3c3af69fcc6d5
Marnu Lombard <marnulombard@gmail.com>
2019-06-02 14:39:07 +0200

    [change] Add code that puts and fetches the results from the cache - Also implement caching logic when fetching rates

--- app/Repositories/CurrencyRepository.php
+++ app/Repositories/CurrencyRepository.php
@@ -8,13 +8,14 @@ use Jimmerioles\BitcoinCurrencyConverter\Exception\ExceptionInterface as Convert
 
 class CurrencyRepository
 {
+    private const CACHE_TAG = 'currency_rates';
+
     const BITCOIN = 'BTC';
     const ETHEREUM = 'ETH';
     const RANDS = 'ZAR';
     const US_DOLLARS = 'USD';
 
     const DEFAULT = self::RANDS;
-
 
     /** @var Converter */
     private $converter;
@@ -24,7 +25,7 @@ class CurrencyRepository
         $this->converter = $converter;
     }
 
-    public function convertCurrency(string $from = self::BITCOIN, string $to = self::DEFAULT, float $amount = 1.00)
+    public function convertCurrency(string $from = self::BITCOIN, string $to = self::DEFAULT, float $amount = 1.00): float
     {
         try {
             if ($to === self::BITCOIN) {
@@ -33,9 +34,36 @@ class CurrencyRepository
                 $converted = $this->converter->toCurrency($to, $amount);
             }
 
+            $this->cacheRate($from, $to, $amount / $converted);
+
             return $converted;
         } catch (ConverterException|GuzzleException $e) {
             \Log::error($e->getMessage(), $e->getTrace());
+
+            return $this->getCachedValue($from, $to, $amount);
         }
+    }
+
+    public function cacheRate(string $from, string $to, float $rate): void
+    {
+        \Cache::tags(self::CACHE_TAG)
+            ->put("$from:$to", $rate, now()->endOfDay());
+        \Cache::tags(self::CACHE_TAG)
+            ->put("$to:$from", 1/$rate, now()->endOfDay());
+
+        return;
+    }
+
+    public function getCachedRate(string $from, string $to): float
+    {
+        return \Cache::tags(self::CACHE_TAG)
+            ->get("$from:$to") ?: 0;
+    }
+
+    public function getCachedValue(string $from, string $to, float $amount): float
+    {
+        $rate = $this->getCachedRate($from, $to);
+
+        return $amount * $rate;
     }
 }
