57f293e4e09cdc8999a6e6297c808ae04f150abb
Marnu Lombard <marnulombard@gmail.com>
2019-06-02 12:56:42 +0200

    [bugfix] Turns out I had a fundamental misunderstanding of `App::when()` That has been fixed.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Jimmerioles\BitcoinCurrencyConverter\Converter;
 use Jimmerioles\BitcoinCurrencyConverter\Provider\CoinbaseProvider;
+use PayBee\Repositories\CurrencyRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,19 +19,12 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         // Bind our crypto currency converter instance into the IOC container
-        $this->app->when(Converter::class)->give(function (Application $app) {
-            return new Converter(new CoinbaseProvider(new Client(), $app->make('cache.store'), 10*60));
-        });
+        $this->app
+            ->when(CurrencyRepository::class)
+            ->needs(Converter::class)
+            ->give(function (Application $app) {
+                return new Converter(new CoinbaseProvider(new Client(), $app->make('cache.store'), 10*60));
+            });
 
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
     }
 }
