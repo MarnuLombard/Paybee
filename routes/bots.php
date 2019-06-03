74641f2a1e1f0d77b05330eb7fe6eb977c77ddc6
Marnu Lombard <marnulombard@gmail.com>
2019-06-03 12:29:13 +0200

    [create] Add a fallback for unrecognised commands

--- routes/bots.php
+++ routes/bots.php
@@ -1,6 +1,8 @@
 <?php
 
+/** @var \BotMan\BotMan\BotMan $botMan */
 $botMan = app('botman');
 
 
 $botMan->hears('/getBTCEquivalent {amount} {currency}', PayBee\Bot\Commands\BtcCommand::class.'@handle');
+$botMan->fallback(PayBee\Bot\Commands\FallBackCommand::class.'@handle');
