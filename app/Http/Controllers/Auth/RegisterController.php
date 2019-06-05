aa74aee035db68c4a0a937da2d478f9a4a7da672
Marnu Lombard <marnulombard@gmail.com>
2019-06-05 08:01:29 +0200

    [change] Store the `User::$default_currency` upon registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'default_currency' => CurrencyRepository::DEFAULT,
         ]);
     }
 
